63b752303b885fa9e1e344dd1a2469e9573b4a06
yamaton <yamaton@gmail.com>
2021-03-04 11:09:42 -0500

    Update week2 of genome-seq

--- 2-genome-sequencing/week1.py
+++ 2-genome-sequencing/week1.py
@@ -1,8 +1,10 @@
 import collections
+import itertools as it
 from typing import Dict, Iterable, List, Set
 import argparse
 from utils import windowed
 
+AdjacencySet = Dict[str, Set[str]]
 AdjacencyList = Dict[str, Set[str]]
 
 
@@ -18,7 +20,7 @@ def genome_path0(shards: Iterable[str]) -> str:
     return "".join(res)
 
 
-def graph_overlap(shards: Iterable[str]) -> AdjacencyList:
+def graph_overlap(shards: Iterable[str]) -> AdjacencySet:
     """
     >>> expected = {"CATGC": {"ATGCG"}, "GCATG": {"CATGC"}, "GGCAT": {"GCATG"}, "AGGCA": {"GGCAC", "GGCAT"}}
     >>> computed = graph_overlap(["ATGCG", "GCATG", "CATGC", "AGGCA", "GGCAT", "GGCAC"])
@@ -36,14 +38,31 @@ def graph_overlap(shards: Iterable[str]) -> AdjacencyList:
     return dict(d)
 
 
+def to_debruijn(text: str, k: int) -> AdjacencyList:
+    """
+    >>> computed = to_debruijn("AAGATTCTCTAAGA", 4)
+    >>> expected = {"AAG":["AGA","AGA"], "AGA":["GAT"], "ATT":["TTC"], "CTA":["TAA"], "CTC":["TCT"], "GAT":["ATT"], "TAA":["AAG"], "TCT":["CTA", "CTC"], "TTC":["TCT"]}
+    >>> computed == expected
+    True
+    """
+    d = collections.defaultdict(list)
+    w1, w2 = it.tee(map(lambda cs: "".join(cs), windowed(text, k-1)))
+    next(w2)
+    for this, that in zip(w1, w2):
+        d[this].append(that)
+
+    return {k: sorted(d[k]) for k in sorted(d.keys())}
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input")
     args = parser.parse_args()
 
     with open(args.input, "r") as f:
-        shards = [s.strip() for s in f.readlines()]
+        k = int(f.readline())
+        text = f.readline().strip()
 
-    res = graph_overlap(shards)
+    res = to_debruijn(text, k)
     for k, v in res.items():
         print(f"{k} -> {','.join(v)}")
