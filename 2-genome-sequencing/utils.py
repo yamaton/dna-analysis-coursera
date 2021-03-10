db20c92cc3ef1bd7959085663d7aabc74297bdb7
yamaton <yamaton@gmail.com>
2021-03-09 23:08:05 -0500

    Update genome-sequencing utils
    Add chunked and helpers

--- 2-genome-sequencing/utils.py
+++ 2-genome-sequencing/utils.py
@@ -1,8 +1,10 @@
 import collections
-from typing import Generator, Iterable
+from functools import partial
+import itertools as it
+from typing import Generator, Iterable, List
 
 
-def windowed(seq: Iterable, n: int, step=1) -> Generator:
+def windowed(iterable: Iterable, n: int, step=1) -> Generator:
     """Returns sliding window as generator
 
     [NOTE] Borrowed from more-itertools
@@ -12,11 +14,41 @@ def windowed(seq: Iterable, n: int, step=1) -> Generator:
     """
     window = collections.deque(maxlen=n)
     i = n
-    for _ in map(window.append, seq):
+    for _ in map(window.append, iterable):
         i -= 1
         if i == 0:
             i = step
             yield tuple(window)
+
+
+def chunked(iterable: Iterable, n: int) -> Generator:
+    """
+    [NOTE] Borrowed from more-itertools
+    >>> list(chunked([1, 2, 3, 4, 5, 6, 7], 3))
+    [[1, 2, 3], [4, 5, 6], [7]]
+    """
+    iterator = iter(partial(take, n, iter(iterable)), [])
+    return iterator
+
+
+def take(n: int, iterable: Iterable) -> List:
+    """
+    >>> take(3, [1, 2, 3,4])
+    [1, 2, 3]
+    """
+    return list(it.islice(iterable, n))
+
+
+
+def reverse_complement(s: str) -> str:
+    """
+    >>> reverse_complement("AAAACCCGGT")
+    'ACCGGGTTTT'
+
+    """
+    assert set(s).issubset(set("ATCG"))
+    d = dict(zip("ATCG", "TAGC"))
+    return "".join(d[x] for x in reversed(s))
 
 
 if __name__ == "__main__":
